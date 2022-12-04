c4519f51dfefdbb26c36227f94270c033910d0b9
Zhenya <zhenya951236qw@mail.ru>
2022-12-05 03:54:18 +0500

    Исправлены недочёты

--- task1/src/Lucky.java
+++ task1/src/Lucky.java
@@ -1,18 +1,18 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Lucky {
-    static int x = 0;
-    static int count = 0;
+    static AtomicInteger luckyX = new AtomicInteger(0);
+    static AtomicInteger count = new AtomicInteger(0);
 
     static class LuckyThread extends Thread {
         @Override
         public  void run() {
-            synchronized (Lucky.class) {
-                while (x < 999999) {
-                    x++;
-                    if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
-                            % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
-                        System.out.println(x);
-                        count++;
-                    }
+            while (luckyX.get() < 999999) {
+                final int x = luckyX.incrementAndGet();
+                if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
+                        % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
+                    System.out.println(x);
+                    count.incrementAndGet();
                 }
             }
         }
